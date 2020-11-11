effb8b3e96d23a36aa330ca62e1a4a36f853d62b
HeYinan123 <56247783+HeYinan123@users.noreply.github.com>
2020-11-12 03:12:37 +0800

    modified Index and Courseclass

--- CZ2002_project/Index.java
+++ CZ2002_project/Index.java
@@ -1,10 +1,76 @@
 import java.util.Queue;
+import java.util.*;
 
 public class Index
 {
-    private int courseId;
-    private int indexNo;
+    private String courseId;  // cz2002
+    private int indexNumber;  // 10203
     private int vacancy;
-    private Queue<String> queue;
-    private Set<String> students;
+    private Time tutorialTime;
+    private Time labTime;
+    private Queue<String> waitlist;  
+    private Set<String> studentList;
+
+    public Index(String courseId, int indexNumber, int vacancy){
+    	this.courseId = courseId;
+    	this.indexNumber = indexNumber;
+    	this.vacancy = vacancy;
+    } 
+
+    public String getCourseId(){
+    	return courseId;
+    }
+
+    public int getIndexNumber(){
+    	return indexNumber;
+    }
+
+    public int getVacancy(){
+    	return vacancy;
+    }
+
+    public Time getTutorialTime(){
+    	return tutorialTime;
+    }
+
+    public Time getLabTime(){
+    	return labTime;
+    }
+
+    public Object[] getWaitList(){
+    	return waitlist.toArray();
+    }
+
+    public Object[] getStudentList(){
+    	return studentList.toArray();
+    }
+
+    public void setCourseId(String courseId){
+    	this.courseId = courseId;
+    }
+
+    public void setIndexNumber(int indexNumber){
+    	this.indexNumber = indexNumber;
+    }
+
+    public void setVacancy(int vacancy){
+    	this.vacancy = vacancy;
+    }
+
+    public void addWaitlist(String ntuAccount){
+    	waitlist.add(ntuAccount);
+    }
+
+    public void removeWaitList(String ntuAccount){
+
+    }
+
+    public void addStudentList(String ntuAccount){
+    	studentList.add(ntuAccount);
+    	vacancy -= 1;
+    }
+
+    public void removeStudentList(String ntuAccount){
+    	studentList.remove(ntuAccount);
+    }
 }
--- CZ2002_project/Time.java
+++ CZ2002_project/Time.java
@@ -1,5 +1,4 @@
 import java.time.DayOfWeek;
-import java.util.Date;
 
 enum Week
 {
@@ -13,3 +12,4 @@ public class Time
 	// 1230 for 12:30 pm, store as [[1230, 1330], [1430, 1530]]
 	int[] time;
 }
+
