bba8beddfcee3f17e0eae15dd0a66748a5c90493
hengwei <bianhwharry@163.com>
2020-11-24 13:40:53 +0800

    fixes script bug

--- MacExecute.sh
+++ MacExecute.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-javac -d ./class -cp ./src:./lib/javax.mail.jar:./lib/javax.activation-1.2.0.jar: ./src/*.java;java -cp ./class:./lib/javax.mail.jar:./lib/javax.activation-1.2.0.jar: Main
+javac -d ./class -cp ./src:./lib/javax.mail.jar:./lib/javax.activation-1.2.0.jar: ./src/*.java;java -cp ./class:./lib/javax.mail.jar:./lib/javax.activation-1.2.0.jar: src.Main
--- WindowsExecute.bat
+++ WindowsExecute.bat
@@ -1,2 +1,2 @@
 javac -d .\class -cp .src/;.\lib\javax.mail.jar;.\lib\javax.activation-1.2.0.jar; .\src\*.java
-java -cp .\class;\lib\javax.mail.jar;\lib\javax.activation-1.2.0.jar; Main
+java -cp .\class;\lib\javax.mail.jar;\lib\javax.activation-1.2.0.jar; src.Main
